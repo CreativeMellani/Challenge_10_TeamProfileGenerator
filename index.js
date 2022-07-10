441c26c5364274eb81f118e6b9f753a4dc80dfe5
CreativeMellani <101590432+CreativeMellani@users.noreply.github.com>
2022-07-10 16:08:57 -0700

    adding inquirer package to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Uses the Inquirer package.
+//Uses the Jest package for a suite of unit tests.
+
+//included inquier packages needed for this  src:https://www.npmjs.com/package/inquirer 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+ 
